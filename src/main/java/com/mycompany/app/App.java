1319ca938b89eedebf5977f87c5bd9da83f80c83
AyseAsude <ayasude99@hotmail.com>
2020-02-07 20:19:48 +0200

    Trying to work the app

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,31 +13,19 @@ import spark.template.mustache.MustacheTemplateEngine;
 
 
 public class App extends Exception {
-    public static void main(String[] args) {
 
-        ArrayList<Integer> list1 = new ArrayList<>();
-        list1.add(5);
-        list1.add(2);
-        list1.add(18);
-        list1.add(20);
-        list1.add(51);
-        list1.add(40);
-        list1.add(3);
-        list1.add(10);
-        list1.add(16);
+    public static int findSum(ArrayList<Integer> list1, ArrayList<Integer> list2, int smallestk1, int smallestk2) throws IllegalArgumentException{
+       if(list1 == null || list2 == null){
+          throw new IllegalArgumentException("arraylist cannot be null");
+        }else if( (smallestk1 > list1.size() || smallestk2 > list2.size()) || (smallestk1 <= 0  || smallestk2 <= 0) ){
+            throw new IllegalArgumentException("keys cannot be smaller or greater than the array's size");
+        }
+        Collections.sort(list1);
+        Collections.sort(list2);
+        return list1.get(smallestk1-1) + list2.get(smallestk2-1);
+    }
 
-        ArrayList<Integer> list2= new ArrayList<>();
-        list2.add(11);
-        list2.add(28);
-        list2.add(43);
-        list2.add(82);
-        list2.add(1);
-        list2.add(9);
-        list2.add(12);
-        list2.add(5);
-        list2.add(8);
-
-        System.out.println(findSum(list1,list2,2,1));
+      public static void main(String[] args) {
         port(getHerokuAssignedPort());
 
         get("/", (req, res) -> "Hello, World");
@@ -88,7 +76,7 @@ public class App extends Exception {
             return new ModelAndView(map, "compute.mustache");
           },
           new MustacheTemplateEngine());
-  }
+        }
 
   static int getHerokuAssignedPort() {
       ProcessBuilder processBuilder = new ProcessBuilder();
@@ -98,15 +86,4 @@ public class App extends Exception {
       return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
    }
 
-
-    public static int findSum(ArrayList<Integer> list1, ArrayList<Integer> list2, int smallestk1, int smallestk2) throws IllegalArgumentException{
-       if(list1 == null || list2 == null){
-          throw new IllegalArgumentException("arraylist cannot be null");
-        }else if( (smallestk1 > list1.size() || smallestk2 > list2.size()) || (smallestk1 <= 0  || smallestk2 <= 0) ){
-            throw new IllegalArgumentException("keys cannot be smaller or greater than the array's size");
-        }
-        Collections.sort(list1);
-        Collections.sort(list2);
-        return list1.get(smallestk1-1) + list2.get(smallestk2-1);
-    }
 }
